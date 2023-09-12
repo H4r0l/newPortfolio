9b9895b9947a744774387a19698ec939ab11c16c
Har0l <65699208+H4r0l@users.noreply.github.com>
2023-09-11 21:47:30 -0500

    Update: Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,51 +1,90 @@
-import { Box, Button, VStack  } from "@chakra-ui/react";
+import { Box, Button, VStack } from "@chakra-ui/react";
 import { motion } from "framer-motion";
 import { Link } from "react-router-dom";
 import Negative from "../assets/Negative.png";
+import { FaUser, FaProjectDiagram, FaAddressCard } from "react-icons/fa";
 
 const Navbar = () => {
-    return (
-      <motion.div
-        initial={{ x: "-100%" }}
-        animate={{ x: "0%" }}
-        exit={{ x: "-200%" }}
-        transition={{ duration: 0.7 }}
+  return (
+    <motion.div
+      initial={{ x: "-100%" }}
+      animate={{ x: "0%" }}
+      exit={{ x: "-200%" }}
+      transition={{ duration: 0.7 }}
+    >
+      <Box
+        bgGradient="linear(to-b, #97E8C0, #D0EAC1)"
+        h={"95vh"} // Adjusted height to 95% of the viewport height
+        w={"7vh"}
+        boxShadow="2px 0px 10px rgba(0, 0, 0, 0.5)"
+        zIndex="10"
+        overflow="hidden"
+        flexDirection="column" // Arrange items vertically
       >
-        <Box
-          bgGradient="linear(to-b, #97E8C0, #D0EAC1)"
-          borderRadius="xl"
-          minH={"95vh"}
-          w={"10vh"}
-          boxShadow="2px 0px 10px rgba(0, 0, 0, 0.5)"
-          position="fixed"
-          left="0"
-          top="0"
-          zIndex="10"
-          overflow="hidden"
+        <VStack
+          p={4}
+          h="100%"
+          justifyContent="space-between"
+          flexGrow={1}
+          pb="65vh"
         >
-          <VStack p={4}>
-            <Button
-              as={Link}
-              to="/"
-              bg="#578A76"
-              rounded="full"
-              textColor={"white"}
-            >
-              <img src={Negative} alt="" width={"16px"} height={"16px"} />
-            </Button>
-            <Button as={Link} to="/about" bg="#578A76" rounded="full">
-              A
-            </Button>
-            <Button as={Link} to="/projects" bg="#578A76" rounded="full">
-              P
-            </Button>
-            <Button as={Link} to="/contact" bg="#578A76" rounded="full">
-              C
-            </Button>
-          </VStack>
-        </Box>
-      </motion.div>
-    );
+          <Button
+            as={Link}
+            to="/"
+            bg="#578A76"
+            rounded="full"
+            textColor={"white"}
+            style={{
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+            p={1}
+            _hover={"blue"}
+          >
+            <img
+              src={Negative}
+              alt=""
+              width={"16px"}
+              height={"16px"}
+              style={{ margin: 0 }}
+            />
+          </Button>
+          <Button
+            as={Link}
+            to="/about"
+            bg="#578A76"
+            rounded="full"
+            style={{ borderRadius: "50%" }}
+            p={2}
+          >
+            <FaUser size={16} color="black" />
+          </Button>
+          <Button
+            as={Link}
+            to="/projects"
+            bg="#578A76"
+            rounded="full"
+            style={{ borderRadius: "50%" }}
+            p={2}
+            alignContent={"center"}
+          >
+            <FaProjectDiagram size={16} color="black" />
+          </Button>
+          <Button
+            as={Link}
+            to="/contact"
+            bg="#578A76"
+            rounded="full"
+            style={{ borderRadius: "50%" }}
+            p={2}
+          >
+            <FaAddressCard size={16} color="black" />
+          </Button>
+        </VStack>
+      </Box>
+    </motion.div>
+  );
 };
 
 export default Navbar;
