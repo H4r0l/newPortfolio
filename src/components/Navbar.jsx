dcac02d716f1073375839ad48a3da0e053d16400
Har0l <haroldurrea6@gmail.com>
2024-04-24 22:12:59 -0500

    Remotion of code blocks before doing big update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,71 +9,7 @@ const Navbar = () => {
   const { colorMode, toggleColorMode } = useColorMode();
 
   return (
-    <motion.div
-      initial={{ bgGradient: "#97E8C0", opacity: 0 }}
-      animate={{
-        bgGradient: colorMode === "light" ? "#D0EAC1" : "#740F65",
-        opacity: 1,
-      }}
-      transition={{ duration: 10, ease: "easeOut" }}
-      variants={{ exit: { opacity: 0 } }}
-    >
-      <Box
-        bgGradient={
-          colorMode === "light"
-            ? "linear(to-b, #97E8C0, #D0EAC1)"
-            : "linear(to-b, #990F71, #740F65)"
-        }
-        h={"100%"}
-        w={"70%"}
-        maxW={"70%"}
-        boxShadow="2px 0px 10px rgba(0, 0, 0, 0.5)"
-        zIndex="10"
-        overflow="hidden"
-        flexDirection="column"
-      >
-        <VStack
-          p={4}
-          h="100%"
-          justifyContent="space-between"
-          flexGrow={1}
-          pb="65vh"
-        >
-          <Button
-            as={Link}
-            to="/"
-            bg="transparent"
-            boxShadow="dark-lg"
-            rounded="full"
-            textColor="black"
-            style={{
-              display: "flex",
-              justifyContent: "center",
-              alignItems: "center",
-            }}
-            p={1}
-            _hover={"blue"}
-          >
-            <img
-              src={Negative}
-              filter="invert(100)"
-              alt=""
-              width={"16px"}
-              height={"16px"}
-              style={{ margin: 0 }}
-            />
-          </Button>
-          <motion.button
-            whileTap={{ scale: 0.95, rotate: 15 }}
-            animate={{ x: 0, rotate: 0 }}
-            transition={{ duration: 0.4, ease: "easeOut" }}
-            variants={{ exit: { x: 20 } }}
-            onClick={toggleColorMode}
-          >
-            {colorMode === "light" ? <FaMoon /> : <FaSun />}
-          </motion.button>
-        </VStack>
-      </Box>
+    <motion.div>
     </motion.div>
   );
 };
